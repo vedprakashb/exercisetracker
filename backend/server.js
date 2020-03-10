5c7d74b62511d6fd62f447f6c8eae4c594206048
Ved <beheraved@gmail.com>
2020-03-11 00:47:14 +0530

    connection string updated

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,15 @@ app.use(cors());
 app.use(express.json());
 
  const uri = process.env.ATLAS_URI;
+ console.log(uri);
  mongoose.connect(uri, {useNewUrlParser : true, useCreateIndex: true ,useUnifiedTopology : true});
 
  const connection = mongoose.connection;
+ //console.log(connection);
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
+connection.on('error', (e) => console.log(e));
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
