a5fb4703ea743266d3afb0d892b60d8707b91d01
Ved <beheraved@gmail.com>
2020-03-11 08:42:27 +0530

    rest api added

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,21 @@ app.use(cors());
 app.use(express.json());
 
  const uri = process.env.ATLAS_URI;
- console.log(uri);
+ 
  mongoose.connect(uri, {useNewUrlParser : true, useCreateIndex: true ,useUnifiedTopology : true});
 
  const connection = mongoose.connection;
- //console.log(connection);
+ 
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 connection.on('error', (e) => console.log(e));
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
